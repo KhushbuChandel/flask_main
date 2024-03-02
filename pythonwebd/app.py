c0df9582283f451e0f2164ce0ab142d2b933cf53
KhushbuChandel <chandelkhushbu773@gmail.com>
2024-03-02 19:58:48 +0530

    done

--- pythonwebd/app.py
+++ pythonwebd/app.py
@@ -3,39 +3,36 @@ from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
 app = Flask(__name__)
-#intialise the sqlalchemy
+# Initialize the SQLAlchemy
 app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///database.sqlite3"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 app.app_context().push()
 
 class Todo(db.Model):
-    
     sno = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(200), nullable=False)
     desc = db.Column(db.String(500), nullable=False)
     date_created = db.Column(db.DateTime, default=datetime.utcnow)
 
-    def _repr_(self):
-        return "Hello"
+    def __repr__(self):
+        return f"Todo('{self.title}', '{self.desc}')"
     
 @app.route('/')
 def hello_world():
-
     todo = Todo(title="First Todo", desc="Start investing in Stock market")
     db.session.add(todo) 
     db.session.commit()
     allTodo = Todo.query.all()
-    return render_template('index.html')
+    return render_template('index.html', allTodo=allTodo)
 
-# @app.route('/products')
 @app.route('/show')
 def products():
     allTodo = Todo.query.all()
     print(allTodo)
-    return 'this is products page'
+    return 'This is the products page'
 
-if __name__ == "_main_":
+if __name__ == "__main__":
     # with app.app_context():
     #     # Create the database tables if they don't exist
     #     db.create_all()
